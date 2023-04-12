84e67299abed31faa9b2e5bfe39337f993bbfdfc
AtmajaDazn <110802676+AtmajaDazn@users.noreply.github.com>
2023-04-12 14:27:11 +0530

    Initial Commit

---
+++ MultiColorProgressBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiColorProgressBar
+//
+//  Created by Atamaja Sahoo on 12/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
